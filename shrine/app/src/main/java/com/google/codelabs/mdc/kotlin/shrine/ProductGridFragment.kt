68ca3a689c7cfc888d4fc4220f0d82f0ee204228
Flávio Kreis <flaviokreis@gmail.com>
2020-10-25 10:09:36 -0300

    Improve to use MaterialToolbar instead Toolbar

--- shrine/app/src/main/java/com/google/codelabs/mdc/kotlin/shrine/ProductGridFragment.kt
+++ shrine/app/src/main/java/com/google/codelabs/mdc/kotlin/shrine/ProductGridFragment.kt
@@ -1,31 +1,14 @@
 package com.google.codelabs.mdc.kotlin.shrine
 
 import android.os.Bundle
-import android.view.*
-import androidx.appcompat.app.AppCompatActivity
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
 import androidx.fragment.app.Fragment
-import kotlinx.android.synthetic.main.shr_product_grid_fragment.view.*
 
 class ProductGridFragment : Fragment() {
-
-    override fun onCreate(savedInstanceState: Bundle?) {
-        super.onCreate(savedInstanceState)
-        setHasOptionsMenu(true)
-    }
-
     override fun onCreateView(
             inflater: LayoutInflater, container: ViewGroup?, savedInstanceState: Bundle?): View? {
-        // Inflate the layout for this fragment
-        val view = inflater.inflate(R.layout.shr_product_grid_fragment, container, false)
-
-        // Set up the toolbar.
-        (activity as AppCompatActivity).setSupportActionBar(view.app_bar)
-
-        return view
-    }
-
-    override fun onCreateOptionsMenu(menu: Menu, menuInflater: MenuInflater) {
-        menuInflater.inflate(R.menu.shr_toolbar_menu, menu)
-        super.onCreateOptionsMenu(menu, menuInflater)
+        return inflater.inflate(R.layout.shr_product_grid_fragment, container, false)
     }
 }
