218373c998c01ff4580fc62401dda859ba3b333e
Flávio Kreis <flaviokreis@gmail.com>
2020-10-25 10:31:39 -0300

    Add list of card views to products

--- shrine/app/src/main/java/com/google/codelabs/mdc/kotlin/shrine/ProductGridFragment.kt
+++ shrine/app/src/main/java/com/google/codelabs/mdc/kotlin/shrine/ProductGridFragment.kt
@@ -5,10 +5,37 @@ import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
 import androidx.fragment.app.Fragment
+import androidx.recyclerview.widget.GridLayoutManager
+import androidx.recyclerview.widget.RecyclerView
+import com.google.codelabs.mdc.kotlin.shrine.network.ProductEntry
+import kotlinx.android.synthetic.main.shr_product_grid_fragment.view.*
 
 class ProductGridFragment : Fragment() {
     override fun onCreateView(
             inflater: LayoutInflater, container: ViewGroup?, savedInstanceState: Bundle?): View? {
-        return inflater.inflate(R.layout.shr_product_grid_fragment, container, false)
+        // Inflate the layout for this fragment with the ProductGrid theme
+        val view = inflater.inflate(R.layout.shr_product_grid_fragment, container, false)
+
+        // Set up the RecyclerView
+        setUpRecyclerView(view)
+
+        return view
+    }
+
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+
+        val products = ProductEntry.initProductEntryList(resources)
+        view.recycler_view.adapter = ProductCardRecyclerViewAdapter(products)
+    }
+
+    private fun setUpRecyclerView(view: View) {
+        view.recycler_view.setHasFixedSize(true)
+        view.recycler_view.layoutManager = GridLayoutManager(context, 2, RecyclerView.VERTICAL, false)
+
+        val largePadding = resources.getDimensionPixelSize(R.dimen.shr_product_grid_spacing)
+        val smallPadding = resources.getDimensionPixelSize(R.dimen.shr_product_grid_spacing_small)
+
+        view.recycler_view.addItemDecoration(ProductGridItemDecoration(largePadding, smallPadding))
     }
 }
